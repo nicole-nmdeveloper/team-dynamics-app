f2f9805733a9cc70aa22e582a2e333abcbce1822
Nicole <102914172+nicole-nmdeveloper@users.noreply.github.com>
2025-04-19 20:40:18 -0300

    Criação dos arquivos env_example e setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Iniciando configurações do projeto Team Dynamics App..."
+
+if [ ! -f .env ]; then
+echo "Criando arquivo .env a partir de env_example..."
+cp env_example .env
+fi
+
+echo "Fazendo o build das imagens Docker..."
+docker-compose build
+
+echo "Subindo containers..."
+docker-compose up -d
+
+echo "Inicializando banco de dados..."
+sleep 5
+
+echo "Criando banco de dados e rodando migrations..."
+docker-compose exec web rails db:create db:migrate
+
+echo "Tudo pronto! Acesse o App em [http://localhost:3000](http://localhost:3000/)"
