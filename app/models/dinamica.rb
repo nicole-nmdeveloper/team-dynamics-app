c2e4dd6f6e7fb68af26a7b82070f08ed1f502926
Nicole <102914172+nicole-nmdeveloper@users.noreply.github.com>
2025-04-19 17:26:51 -0300

    Alteração para permitir que uma dinâmica seja cadastrada sem uma review

--- app/models/dinamica.rb
+++ app/models/dinamica.rb
@@ -1,8 +1,13 @@
 class Dinamica < ApplicationRecord
   has_many :reviews, inverse_of: :dinamica, dependent: :destroy
-  accepts_nested_attributes_for :reviews, allow_destroy: true
+  accepts_nested_attributes_for :reviews, allow_destroy: true, reject_if: :review_invalida?
 
   def nota_media
     reviews.average(:nota)&.round(2)
+  end
+
+  private
+  def review_invalida?(attributes)
+    attributes['comentario'].blank? && attributes['nota'].blank?
   end
 end
